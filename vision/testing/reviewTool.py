f5635b5ebfa1795a1e0f25cad21d285f460c8f01
jsf2408 <jsf2408@gmail.com>
2019-03-09 22:44:12 +0000

    modified:   reviewTool.py

--- vision/testing/reviewTool.py
+++ vision/testing/reviewTool.py
@@ -2,8 +2,27 @@ import numpy as np
 import cv2
 import os
 
-x = None
-y = None
+def clickEvent(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+                global pointX,pointY,count,depthMap,pathData
+                pointX = x
+                pointY = y
+                depthMapCross = cv2.line(depthMap,(x-5,y),(x+5,y),(0,255,0),1)
+                depthMapCross = cv2.line(depthMap,(x,y-5),(x,y+5),(0,255,0),1)
+                cv2.imshow('map', depthMapCross)
+                
+        if event == cv2.EVENT_LBUTTONUP and pointX != None:
+                print(pointX, pointY)
+                count = count + 1
+                imageName = fileName0 + '--' + format(count, '05') + '.png'
+                file.write(imageName + '\t' + str(pointX) + '\t' + str(pointY) + '\n')
+
+                cv2.imwrite(os.path.join(pathData, imageName), depthMap)
+
+                retMap, depthMap = capDepth.read()
+
+pointX = None
+pointY = None
 count = 0
 
 pathVideo = 'videos/stereoImage'
@@ -11,36 +30,19 @@ pathData = 'trainingData'
 fileName0 = 'output'
 fileName = fileName0 + '.avi'
 
-capDepth  = cv2.VideoCapture(os.path.join(pathVideo))
+textFile = 'trainingData.txt'
+file = open(os.path.join(pathData, textFile),'a')
+capDepth  = cv2.VideoCapture(os.path.join(pathVideo,fileName))
+
 retMap, depthMap = capDepth.read()
-
-def clickEvent(event, x, y, flags, param, count):
-        if event == cv2.EVENT_LBUTTONDOWN:
-                print('1')
-                print(x, y)
-                count = count + 1
-                imageName = fileName0 + '--' + format(count, '05') + '.png'
-
-                print(imageName)
-
-                cv2.imwrite(os.path.join(pathData, imageName), depthMap)
-
-                retMap, depthMap = capDepth.read()
-
-                
+cv2.namedWindow('map')
+cv2.setMouseCallback('map', clickEvent)
 
 while(retMap == True):
-
-# Display the resulting frame
 	cv2.imshow('map', depthMap)
-
-        print('1')
-        print(x,y)
-	cv2.setMouseCallback('map', clickEvent)
-	print('2')
+	if cv2.waitKey(1) == 27:
+		retMap, depthMap = capDepth.read()
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
-        print('3')
-# When everything done, release the capture
 capDepth.release()
 cv2.destroyAllWindows()
