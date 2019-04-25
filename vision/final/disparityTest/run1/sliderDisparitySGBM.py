aae91eaf403a26de480f0fd856a5ddee4e97b421
jsf2408 <jsf2408@gmail.com>
2019-04-25 23:07:19 +0100

    modified:   vision/final/disparityTest/run1/sliderDisparitySGBM.py

--- vision/final/disparityTest/run1/sliderDisparitySGBM.py
+++ vision/final/disparityTest/run1/sliderDisparitySGBM.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
+from timeit import default_timer as timer
 
 def nothing(x):
     pass
 
 cv2.namedWindow('image')
 
-imageCombined  = cv2.imread('720_2019-04-23-13-47-42.png')
-calibration = np.load("720-10-calibration.npz")
+imageCombined  = cv2.imread('480_2019-04-23-13-47-42.png')
+calibration = np.load("480-10-calibration.npz")
 imageSize = tuple(calibration["imageSize"])
 xMapLeft = calibration["xMapLeft"]
 yMapLeft = calibration["yMapLeft"]
@@ -17,29 +18,31 @@ yMapRight = calibration["yMapRight"]
 # create trackbars for color change
 cv2.createTrackbar('1','image',0,10,nothing)
 cv2.createTrackbar('2','image',0,5,nothing)
-cv2.createTrackbar('3','image',5,250,nothing)
+cv2.createTrackbar('3','image',5,20,nothing)
 cv2.createTrackbar('4','image',600,10000,nothing)
 cv2.createTrackbar('5','image',2400,10000,nothing)
 cv2.createTrackbar('6','image',20,255,nothing)
-cv2.createTrackbar('7','image',16,255,nothing)
+cv2.createTrackbar('7','image',0,255,nothing)
 cv2.createTrackbar('8','image',1,255,nothing)
-cv2.createTrackbar('9','image',100,255,nothing)
-cv2.createTrackbar('10','image',20,255,nothing)
+cv2.createTrackbar('9','image',0,255,nothing)
+cv2.createTrackbar('10','image',0,255,nothing)
 cv2.createTrackbar('11','image',0,1,nothing)
 
 WIDTH = imageCombined.shape[1]/2
 
-imageLeft = imageCombined[:, 0:WIDTH]
-imageRight = imageCombined[:, WIDTH:2*WIDTH]
+imageLeft0 = imageCombined[:, 0:WIDTH]
+imageRight0 = imageCombined[:, WIDTH:2*WIDTH]
 
-realLeft = cv2.remap(imageLeft, xMapLeft, yMapLeft, cv2.INTER_LINEAR)
-realRight = cv2.remap(imageRight, xMapRight, yMapRight, cv2.INTER_LINEAR)
 
-imageLeft = cv2.cvtColor(realLeft, cv2.COLOR_BGR2GRAY)
-imageRight = cv2.cvtColor(realRight, cv2.COLOR_BGR2GRAY)
-
-cv2.imshow('reall', realLeft)
-cv2.imshow('realr', realRight)
+##realLeft = cv2.remap(imageLeft, xMapLeft, yMapLeft, cv2.INTER_LINEAR)
+##realRight = cv2.remap(imageRight, xMapRight, yMapRight, cv2.INTER_LINEAR)
+##
+##imageLeft = cv2.cvtColor(realLeft, cv2.COLOR_BGR2GRAY)
+##imageRight = cv2.cvtColor(realRight, cv2.COLOR_BGR2GRAY)
+##
+##
+##cv2.imshow('reall', realLeft)
+##cv2.imshow('realr', realRight)
 
 while(1):
     # get current positions of four trackbars
@@ -64,13 +67,22 @@ while(1):
                                        preFilterCap, uniquenessRatio, \
                                        speckleWindowSize, speckleWindowSize, \
                                        doublePass)
-    
-    disparityMap = stereosgbm.compute(imageLeft,imageRight)
+    time = 0
+    for i in range(10):
+        start = timer()
+        realLeft = cv2.remap(imageLeft0, xMapLeft, yMapLeft, cv2.INTER_LINEAR)
+        realRight = cv2.remap(imageRight0, xMapRight, yMapRight, cv2.INTER_LINEAR)
+
+        imageLeft = cv2.cvtColor(realLeft, cv2.COLOR_BGR2GRAY)
+        imageRight = cv2.cvtColor(realRight, cv2.COLOR_BGR2GRAY)
+        
+        disparityMap = stereosgbm.compute(imageLeft,imageRight)
+        time = (timer() - start + time)/2
     disparityMap = cv2.resize(disparityMap, (320, 240))
     disparityMap = np.uint8(disparityMap)
     
     cv2.imshow('map',disparityMap)
-    print(disparityMap[0,0])
+    print(time)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
